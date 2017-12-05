b32ca0e1b9411589834ca4c67f391514b1eb8c3a
Mikael Krief <krief_mikael@hotmail.com>
2017-12-05 22:21:59 +0100

    first commit

---
+++ GitHubTasks/GitHubTag/GitHubTag.ps1
@@ -0,0 +1,27 @@
+#
+# GitHubTag.ps1
+#
+[CmdletBinding(DefaultParameterSetName = 'None')]
+param(
+	[string][Parameter(Mandatory=$true)] $variable1, 
+	[string] $variable2
+)
+
+Write-Host "Starting GitHubTag"
+
+#Run Save-Module -Name VstsTaskSdk -Path .\ for get the Powershell VSTS SDK
+# see https://github.com/Microsoft/vsts-task-lib/tree/master/powershell/Docs
+#Trace-VstsEnteringInvocation $MyInvocation
+
+try {
+Write-Host "variable 1: "$variable1
+Write-Host "variable 2: "$variable2
+
+
+} catch {
+
+} finally {
+	#Trace-VstsLeavingInvocation $MyInvocation
+}
+
+Write-Host "Ending GitHubTag"
